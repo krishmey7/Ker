5eb1f1c7665ec5894a361cc49d42fd5c87ac8021
Krishna <krishnameya96@gmail.com>
2026-07-26 20:06:13 +0100

    Temp fake migration for payments 0006

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+# ⚠️ Ligne temporaire pour contourner le conflit d'index
+python manage.py migrate payments 0006_kibawallet_fields --fake
+
+# Migration normale pour le reste
 python manage.py migrate
